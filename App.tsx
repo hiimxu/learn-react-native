87cdf3b3f089726fd09198fd42af8705e74138a5
HieuDD <mhoguty0213@gmail.com>
2022-12-07 15:05:21 +0700

    Init project

--- App.tsx
+++ App.tsx
@@ -1,22 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider } from 'react-native-elements';
+import { NavigationContainer } from '@react-navigation/native';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+//SCREEN
+import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+        <SafeAreaProvider>
+            <ThemeProvider>
+                <NavigationContainer>
+                    <HomeScreen />
+                    <StatusBar />
+                </NavigationContainer>
+            </ThemeProvider>
+        </SafeAreaProvider>
     );
-  }
 }
