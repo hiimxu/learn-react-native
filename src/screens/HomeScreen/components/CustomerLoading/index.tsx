0283b2b24e61ab508bbf85ffb180227e425fadc9
HieuDD <mhoguty0213@gmail.com>
2022-12-16 14:26:23 +0700

    Fixed UI loading Customer Screen

--- src/screens/HomeScreen/components/CustomerLoading/index.tsx
+++ src/screens/HomeScreen/components/CustomerLoading/index.tsx
@@ -1,29 +1,98 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { Skeleton } from '@rneui/themed';
-import { VStack } from '@react-native-material/core';
+import { Box, HStack, VStack } from '@react-native-material/core';
 import styled from 'styled-components';
 
 const Wrapper = styled(VStack)`
     padding: 10px;
 `;
+const LoadingListItemName = styled(Skeleton)`
+    width: 200px;
+    height: 20px;
+    border-radius: 10px;
+`;
 
-const LoadingListItem = styled(Skeleton)`
-    height: 70px;
+const LoadingListItemInfo = styled(Skeleton)`
+    height: 20px;
     border-radius: 10px;
 `;
 
 export function CustomerLoading() {
     return (
-        <Wrapper spacing={20}>
-            <LoadingListItem />
-            <LoadingListItem />
-            <LoadingListItem />
-            <LoadingListItem />
-            <LoadingListItem />
-            <LoadingListItem />
-            <LoadingListItem />
-            <LoadingListItem />
+        <Wrapper spacing={25}>
+            <HStack spacing={10}>
+                <Skeleton circle width={50} height={50} />
+                <Box style={{ flex: 1 }}>
+                    <VStack spacing={10}>
+                        <LoadingListItemName />
+                        <LoadingListItemInfo />
+                    </VStack>
+                </Box>
+            </HStack>
+            <HStack spacing={10}>
+                <Skeleton circle width={50} height={50} />
+                <Box style={{ flex: 1 }}>
+                    <VStack spacing={15}>
+                        <LoadingListItemName />
+                        <LoadingListItemInfo />
+                    </VStack>
+                </Box>
+            </HStack>
+            <HStack spacing={10}>
+                <Skeleton circle width={50} height={50} />
+                <Box style={{ flex: 1 }}>
+                    <VStack spacing={15}>
+                        <LoadingListItemName />
+                        <LoadingListItemInfo />
+                    </VStack>
+                </Box>
+            </HStack>
+            <HStack spacing={10}>
+                <Skeleton circle width={50} height={50} />
+                <Box style={{ flex: 1 }}>
+                    <VStack spacing={15}>
+                        <LoadingListItemName />
+                        <LoadingListItemInfo />
+                    </VStack>
+                </Box>
+            </HStack>
+            <HStack spacing={10}>
+                <Skeleton circle width={50} height={50} />
+                <Box style={{ flex: 1 }}>
+                    <VStack spacing={15}>
+                        <LoadingListItemName />
+                        <LoadingListItemInfo />
+                    </VStack>
+                </Box>
+            </HStack>
+            <HStack spacing={10}>
+                <Skeleton circle width={50} height={50} />
+                <Box style={{ flex: 1 }}>
+                    <VStack spacing={15}>
+                        <LoadingListItemName />
+                        <LoadingListItemInfo />
+                    </VStack>
+                </Box>
+            </HStack>
+            <HStack spacing={10}>
+                <Skeleton circle width={50} height={50} />
+                <Box style={{ flex: 1 }}>
+                    <VStack spacing={15}>
+                        <LoadingListItemName />
+                        <LoadingListItemInfo />
+                    </VStack>
+                </Box>
+            </HStack>
+            <HStack spacing={10}>
+                <Skeleton circle width={50} height={50} />
+                <Box style={{ flex: 1 }}>
+                    <VStack spacing={15}>
+                        <LoadingListItemName />
+                        <LoadingListItemInfo />
+                    </VStack>
+                </Box>
+            </HStack>
         </Wrapper>
     );
 }
