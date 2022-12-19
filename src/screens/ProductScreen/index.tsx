50f958f1adf1bffdc4a8a132344c46acb12f826c
HieuDD <mhoguty0213@gmail.com>
2022-12-19 15:18:02 +0700

    Create UI list product

--- src/screens/ProductScreen/index.tsx
+++ src/screens/ProductScreen/index.tsx
@@ -1,16 +1,58 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import React from 'react';
-import { Button } from 'react-native-elements';
-import { RootStackParams } from '../../models/route';
+
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 
+//components
+import ProductItem from './components/ProductItem';
+
+//typpes
+import { RootStackParams } from '../../models/route';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProductSelector } from '../../redux/selectors/productSelector';
+import { getListProduct } from '../../redux/actions/creators/product';
+import { authSelector } from '../../redux/selectors/authSelector';
+import { BottomSheet, ListItem } from 'react-native-elements';
+
+type Params = {
+    strFromDate: string;
+    strToDate: string;
+    status: number;
+    name: string;
+};
+
 export default function ProductScreen() {
+    //State
+    const [params, setParams] = React.useState<Params>();
+
+    const [isVisible, setIsVisible] = React.useState(false);
+
+    //Redux State
+    const { account } = useSelector(authSelector);
+    const { loading, data: productList } = useSelector(listProductSelector);
+
+    //Redux hooks
+    const dispatch = useDispatch();
+
+    //Get list product
+    React.useEffect(() => {
+        dispatch(getListProduct(account.id_token, params));
+    }, [params]);
+
     const { navigate } = useNavigation<StackNavigationProp<RootStackParams>>();
 
     return (
-        <View>
-            <Text>ProductScreen</Text>
-        </View>
+        <React.Fragment>
+            {loading ? (
+                <Text>Loading...</Text>
+            ) : (
+                <ScrollView>
+                    {productList?.map((product: any) => (
+                        <ProductItem key={product?.id} product={product} />
+                    ))}
+                </ScrollView>
+            )}
+        </React.Fragment>
     );
 }
